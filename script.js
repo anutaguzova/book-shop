406218c66c448b7674f47719df23d261d9b603ac
Anna Guzova <36640991+anutaguzova@users.noreply.github.com>
2022-05-22 18:35:40 +0200

    feat: add correct button more

--- script.js
+++ script.js
@@ -2,26 +2,11 @@
 let catalogElement = document.createElement('div');
 catalogElement.className = "catalog";
 
-
-
-let modalElement = document.createElement('div');
-let overlayElement = document.createElement('div');
-let btnClose = document.createElement('button');
-let descriptionElement = document.createElement('div');
-modalElement.className = "modal";
-descriptionElement.className = "book_description";
-btnClose.className = "close-button";
-modalElement.id = "modal";
-overlayElement.id = "overlay";
-btnClose.setAttribute("data-close-button", '');
-btnClose.innerText = 'X';
-
-modalElement.append(btnClose, descriptionElement);
-
 fetch('./assets/book.json')
     .then((response) => response.json())
     .then((data) => {
         for (let i = 0; i < data.length; i++) {
+            // book
             let bookElement = document.createElement('div');
             let bookWrapper = document.createElement('div');
             let bookImage = document.createElement('img');
@@ -30,7 +15,6 @@ fetch('./assets/book.json')
             let bookPrice = document.createElement('p');
             let btnMore = document.createElement('a');
             let btnBacket = document.createElement('button');
-
 
             bookElement.className = "book";
             bookWrapper.className = "book_wrapper";
@@ -41,81 +25,42 @@ fetch('./assets/book.json')
             btnMore.className = "button more__button";
             btnBacket.className = "button backet__button";
 
-            bookImage.src = data[i].imageLink;
-
-
-
-
-            btnMore.setAttribute("type", "button");
-            btnBacket.setAttribute("type", "button");
-            bookImage.setAttribute("alt", "book");
-            btnMore.setAttribute("data-modal-target", "#modal");
-            btnMore.setAttribute("href", "#popup1");
-            // descriptionElement.innerText = data[i].description;
-
-
             bookTitle.innerText = data[i].title;
             bookAuthor.innerText = data[i].author;
             bookPrice.innerText = data[i].price;
             btnMore.innerText = "Learn more";
             btnBacket.innerText = "Add to backet";
+            bookImage.src = data[i].imageLink;
 
+            btnMore.setAttribute("type", "button");
+            btnBacket.setAttribute("type", "button");
+            bookImage.setAttribute("alt", "book");
 
+            // popup description
+            let popupElement = document.createElement('div');
+            let moreElement = document.createElement('div');
+            let closeElement = document.createElement('div');
+            let descriptionElement = document.createElement('p');
 
-            catalogElement.appendChild(bookElement);
+            popupElement.id = "popup_more";
+            moreElement.id = "more_content";
+            closeElement.className = "close_result";
+            descriptionElement.className = "description";
+            closeElement.innerText = "X";
+
+            moreElement.append(closeElement, descriptionElement);
+            popupElement.appendChild(moreElement);
+
+            catalogElement.append(bookElement, popupElement);
             bookElement.append(bookWrapper, bookTitle, bookAuthor, bookPrice, btnBacket);
             bookWrapper.append(bookImage, btnMore);
 
-            // popup
-            const openModalButtons = document.querySelectorAll('[data-modal-target]')
-            const closeModalButtons = document.querySelectorAll('[data-close-button]')
-
-
-            openModalButtons.forEach(button => {
-                button.addEventListener('click', (e) => {
-                    const modal = document.querySelector(e.target.dataset.modalTarget)
-                    openModal(modal)
-                    descriptionElement.innerText = data[0].description;
-
-                })
-            })
-
-            overlay.addEventListener('click', () => {
-                const modals = document.querySelectorAll('.modal.active')
-                modals.forEach(modal => {
-                    closeModal(modal)
-
-                })
-            })
-
-            closeModalButtons.forEach(button => {
-                button.addEventListener('click', () => {
-                    // const modal = button.closest('.modal')
-                    closeModal(modal)
-                })
-            })
-
-            function openModal(modal) {
-                if (modal == null) return
-                modal.classList.add('active')
-                overlayElement.classList.add('active')
-                //   descriptionElement.innerText = data[i].description;
-            }
-
-            function closeModal(modal) {
-                if (modal == null) return
-                modal.classList.remove('active')
-
-                overlayElement.classList.remove('active')
-                descriptionElement.innerText = ' ';
-            }
-
+            btnMore.addEventListener("click", (e) =>
+                popupElement.classList.add("visible"),
+                descriptionElement.innerText = data[i].description
+            );
+            closeElement.addEventListener("click", () => popupElement.classList.remove("visible"));
         }
-
     });
 
-document.querySelector('.wrapper').append(catalogElement, modalElement, overlayElement);
-
-
-
-
+document.querySelector('.wrapper').appendChild(catalogElement);
